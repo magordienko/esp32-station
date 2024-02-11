3aa2342b936b453ce546dc839aeea086b5345f16
magordienko <mg260599@gmail.com>
2024-02-12 00:38:25 +0300

    Added GPIO configuration

--- main/main.c
+++ main/main.c
@@ -5,44 +5,39 @@
 #include "esp_flash.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
 #include "sdkconfig.h"
 
-/* Print chip information */
-void print_chip_information()
+#define GPIO_2 2
+#define GPIO_4 4
+
+/* */
+static void behavior_gpio(void)
 {
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ", CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154)
-               ? ", 802.15.4 (Zigbee/Thread)"
-               : "");
+    static uint8_t s_led_state = 0;
 
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if (esp_flash_get_size(NULL, &flash_size) != ESP_OK)
-    {
-        printf("Get flash size failed");
-        return;
-    }
+    s_led_state = !s_led_state;
 
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded"
-                                                         : "external");
+    gpio_set_level(GPIO_2, s_led_state);
+    gpio_set_level(GPIO_4, !s_led_state);
+}
 
-    printf("Minimum free heap size: %" PRIu32 " bytes\n",
-           esp_get_minimum_free_heap_size());
+/* */
+static void configure_gpio(void)
+{
+    gpio_reset_pin(GPIO_2);
+    gpio_reset_pin(GPIO_4);
+
+    gpio_set_direction(GPIO_2, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_4, GPIO_MODE_OUTPUT);
 }
 
 void app_main(void)
 {
-    print_chip_information();
+    configure_gpio();
     while (1)
     {
+        behavior_gpio();
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
