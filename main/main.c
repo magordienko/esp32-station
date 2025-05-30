69426ec177e7243cd4074a8ab97cf233c3628bfd
magordienko <mg260599@gmail.com>
2025-05-30 20:08:13 +0300

    Рефакторинг кода

--- main/main.c
+++ main/main.c
@@ -1,52 +1,66 @@
 #include "main.h"
-//------------------------------------------------
-static const char *TAG = "main";
 
-extern uint16_t TFT9341_WIDTH;
-extern uint16_t TFT9341_HEIGHT;
-//------------------------------------------------
-void lcd_spi_pre_transfer_callback(spi_transaction_t *t)
+// Колбэк для SPI (управление DC пином)
+static void spi_pre_transfer_callback(spi_transaction_t *t)
 {
-    int dc = (int)t->user;
-    gpio_set_level(PIN_NUM_DC, dc);
+    int dc_level = (int)t->user;
+    gpio_set_level(PIN_NUM_DC, dc_level);
 }
-//------------------------------------------------
-void app_main(void)
+
+void display_init(const DisplayPins_t *pins, spi_device_handle_t *spi_out)
 {
     esp_err_t ret;
     spi_device_handle_t spi;
-    // Configure SPI bus
-    spi_bus_config_t cfg = {
-        .mosi_io_num = PIN_NUM_MOSI,
-        .miso_io_num = -1,
-        .sclk_io_num = PIN_NUM_CLK,
+
+    // Конфиг SPI шины
+    spi_bus_config_t bus_cfg = {
+        .mosi_io_num = pins->mosi,
+        .miso_io_num = -1, // Не используем MISO
+        .sclk_io_num = pins->clk,
         .quadwp_io_num = -1,
         .quadhd_io_num = -1,
-        .max_transfer_sz = 16 * 320 * 2 + 8,
+        .max_transfer_sz = 16 * 320 * 2 + 8, // DMA-буфер под дисплей
     };
-    ret = spi_bus_initialize(HSPI_HOST, &cfg, SPI_DMA_CH_AUTO);
-    ESP_LOGI(TAG, "spi bus initialize: %d", ret);
-    spi_device_interface_config_t devcfg = {
-        .clock_speed_hz = 32000000,              // Clock out at 10 MHz
-        .mode = 0,                               // SPI mode 0
-        .spics_io_num = PIN_NUM_CS,              // CS pin
-        .queue_size = 7,                         // We want to be able to queue 7 transactions at a time
-        .pre_cb = lcd_spi_pre_transfer_callback, // Specify pre-transfer callback to handle D/C line
+
+    // Инициализация SPI
+    ret = spi_bus_initialize(HSPI_HOST, &bus_cfg, SPI_DMA_CH_AUTO);
+    ESP_ERROR_CHECK(ret);
+
+    // Конфиг устройства (дисплей)
+    spi_device_interface_config_t dev_cfg = {
+        .clock_speed_hz = 32 * 1000 * 1000, // 32 MHz
+        .mode = 0,                          // SPI mode 0
+        .spics_io_num = pins->cs,
+        .queue_size = 7,                     // Размер очереди транзакций
+        .pre_cb = spi_pre_transfer_callback, // Колбэк для DC пина
     };
-    ret = spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
-    ESP_LOGI(TAG, "spi bus add device: %d", ret);
+
+    ret = spi_bus_add_device(HSPI_HOST, &dev_cfg, &spi);
+    ESP_ERROR_CHECK(ret);
+
+    // Инициализация дисплея
     TFT9341_ini(spi, 320, 240);
-    TFT9341_FillScreen(spi, TFT9341_WHITE);
     TFT9341_FillScreen(spi, TFT9341_BLACK);
+
+    *spi_out = spi; // Сохраняем хэндл в переданный указатель
+}
+
+void app_main()
+{
+    DisplayPins_t pins = {18, 23, 27, 26, 25}; // Пины
+    spi_device_handle_t spi;                   // Делаем хэндл SPI глобальным для файла
+
+    // Передаем &spi в display_init, чтобы он его заполнил
+    display_init(&pins, &spi); // Теперь функция принимает два аргумента
+
+    TFT9341_SetRotation(spi, 3);
+    TFT9341_SetTextColor(TFT9341_GREEN);
+    TFT9341_SetBackColor(TFT9341_BLACK);
+    TFT9341_SetFont(&Font12);
+
     while (1)
     {
-        // TFT9341_FillScreen(spi, TFT9341_BLACK);
-        TFT9341_SetRotation(spi, 3);
-        TFT9341_SetTextColor(TFT9341_GREEN);
-        TFT9341_SetBackColor(TFT9341_BLACK);
-        TFT9341_SetFont(&Font12);
-
-        vTaskDelay(18000 / portTICK_PERIOD_MS);
+        // Отрисовка строк с задержкой
         TFT9341_String(spi, 0, num_row(0), "I'm giving you a nightcall to tell you");
         vTaskDelay(5000 / portTICK_PERIOD_MS);
         TFT9341_String(spi, 140, num_row(1), "how I feel.");
--- main/main.h
+++ main/main.h
@@ -1,18 +1,27 @@
-#ifndef MAIN_MAIN_H_
-#define MAIN_MAIN_H_
+#pragma once
 
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/queue.h"
 #include "driver/gpio.h"
-#include <driver/spi_master.h>
-#include "esp_err.h"
+#include "driver/spi_master.h"
 #include "esp_log.h"
-#include "sdkconfig.h"
-
 #include "spi_ili9341.h"
 
 #define num_row(i) i * 12
 
-#endif
+// Конфиг пинов (теперь не хардкод)
+typedef struct
+{
+    int clk;
+    int mosi;
+    int dc;
+    int rst;
+    int cs;
+} DisplayPins_t;
+
+// Лог-тэг
+static const char *TAG = "Display";
+
+// Инициализация дисплея
+void display_init(const DisplayPins_t *pins, spi_device_handle_t *spi_out);
